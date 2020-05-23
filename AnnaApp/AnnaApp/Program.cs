1396d9f6d9080d1d742815b648036a9552bddad8
HannaOstapenko95 <hanna.ostapenko95@gmail.com>
2020-05-23 21:58:08 +0300

    Initial commit

---
+++ AnnaApp/AnnaApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TextWriter textWriter = new StreamWriter(), true);
+            Console.WriteLine("Enter Number of elements:");
+            int numCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> num = new List<int>();
+            Console.WriteLine("Enter elements:");
+            for (int i = 0; i < numCount; i++)
+            {
+                int numItem = Convert.ToInt32(Console.ReadLine().Trim());
+                num.Add(numItem);
+            }
+            Console.WriteLine("Enter K (number of iterations)");
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+            int result = Result.minSum(num, k);
+            //  textWriter.WriteLine(result);
+            Console.WriteLine("Result (sum) =" + result);
+            //textWriter.Flush();
+            //textWriter.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnnaApp/AnnaApp/Result.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnaApp
+{
+    class Result
+    {
+        public static int minSum(List<int> num, int k)
+        {
+            var arr = num.ToArray();
+            int length = num.Count;
+            int index;
+            try
+            {
+                if ((num.Count >= 1 && num.Count <= Math.Pow(10, 5)) && (k >= 1 && k <= Math.Pow(10, 7)))
+                {
+                    for (int i = 1; i <= k; i++)
+                    {
+                        index = i;
+                        if (index > length)
+                        {
+                            index = 1;
+                        }
+                        if (i >= k)
+                        {
+                            index = Array.IndexOf(arr, arr.Max());
+                            Console.WriteLine(index + "max");
+                            GetMinSum(ref num, index);
+                        }
+                        else
+                        {
+                            GetMinSum(ref num, length - index);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            int sum = 0;
+            for (int i = 0; i < num.Count; i++)
+            {
+                sum += num[i];
+            }
+            return sum;
+        }
+
+        private static void GetMinSum(ref List<int> num, int index)
+        {
+            Console.WriteLine("Element to remove" + num[index]);
+            int ceiling = (int)Math.Ceiling((double)num[index] / 2);
+            num.Remove(num[index]);
+            Console.WriteLine("After removal");
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("After adding ceiling");
+            num.Insert(index, ceiling);
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
